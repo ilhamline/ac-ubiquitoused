67dc8c114acc5c8863caab831348542c31369a5e
Ilham Kurniawan <ilhamkurni@gmail.com>
2016-12-11 21:00:52 +0700

    id = 0 for all ac affected

--- index.php
+++ index.php
@@ -25,31 +25,103 @@ $app->get('/index', function () use ($conn){
 	$app->response()->headers->set('Content-Type', 'application/json');
 	switch ($func) {
 		case 'get_status':
+			if ($id == 0) {
+				$sql = "SELECT id FROM ac";
+				$result = $conn->query($sql);
+				$num_rows = $result->num_rows;
+				for ($i=1; $i <= $num_rows; $i++) {
+					echo json_encode(getStatus($conn, $i), JSON_PRETTY_PRINT);
+				}
+				return;
+			}
 			echo json_encode(getStatus($conn, $id), JSON_PRETTY_PRINT);
 			break;
 		case 'set_status':
 			$status = $app->request->get('arg1');
+			if ($id == 0) {
+				$sql = "SELECT id FROM ac";
+				$result = $conn->query($sql);
+				$num_rows = $result->num_rows;
+				for ($i=1; $i <= $num_rows; $i++) {
+					echo json_encode(setStatus($conn, $i, $status), JSON_PRETTY_PRINT);
+				}
+				return;
+			}
 			echo json_encode(setStatus($conn, $id, $status), JSON_PRETTY_PRINT);
 			break;
 		case 'set_temperature':
 			$temp = $app->request->get('arg1');
+			if ($id == 0) {
+				$sql = "SELECT id FROM ac";
+				$result = $conn->query($sql);
+				$num_rows = $result->num_rows;
+				for ($i=1; $i <= $num_rows; $i++) {
+					echo json_encode(setTemp($conn, $i, $temp ), JSON_PRETTY_PRINT);
+				}
+				return;
+			}
 			echo json_encode(setTemp($conn, $id, $temp ), JSON_PRETTY_PRINT);
 			break;
 		case 'get_temperature':
+			if ($id == 0) {
+				$sql = "SELECT id FROM ac";
+				$result = $conn->query($sql);
+				$num_rows = $result->num_rows;
+				for ($i=1; $i <= $num_rows; $i++) {
+					echo json_encode(getTemp($conn, $i), JSON_PRETTY_PRINT);
+				}
+				return;
+			}
 			echo json_encode(getTemp($conn, $id), JSON_PRETTY_PRINT);
 			break;
 		case 'set_timer':
 			$action = $app->request->get('arg1');
 			$time = $app->request->get('arg2');
+			if ($id == 0) {
+				$sql = "SELECT id FROM ac";
+				$result = $conn->query($sql);
+				$num_rows = $result->num_rows;
+				for ($i=1; $i <= $num_rows; $i++) {
+					echo json_encode(setTimer($conn, $i, $action, $time), JSON_PRETTY_PRINT);
+				}
+				return;
+			}
 			echo json_encode(setTimer($conn, $id, $action, $time), JSON_PRETTY_PRINT);
 			break;
 		case 'reset_timer':
+			if ($id == 0) {
+				$sql = "SELECT id FROM ac";
+				$result = $conn->query($sql);
+				$num_rows = $result->num_rows;
+				for ($i=1; $i <= $num_rows; $i++) {
+					echo json_encode(resetTimer($conn, $i), JSON_PRETTY_PRINT);
+				}
+				return;
+			}
 			echo json_encode(resetTimer($conn, $id), JSON_PRETTY_PRINT);
 			break;
 		case 'get_timer':
+			if ($id == 0) {
+				$sql = "SELECT id FROM ac";
+				$result = $conn->query($sql);
+				$num_rows = $result->num_rows;
+				for ($i=1; $i <= $num_rows; $i++) {
+					echo json_encode(getTimer($conn, $i), JSON_PRETTY_PRINT);
+				}
+				return;
+			}
 			echo json_encode(getTimer($conn, $id), JSON_PRETTY_PRINT);
 			break;
 		case 'get_running_time':
+			if ($id == 0) {
+				$sql = "SELECT id FROM ac";
+				$result = $conn->query($sql);
+				$num_rows = $result->num_rows;
+				for ($i=1; $i <= $num_rows; $i++) {
+					echo json_encode(getRunningTime($conn, $i), JSON_PRETTY_PRINT);
+				}
+				return;
+			}
 		  echo json_encode(getRunningTime($conn, $id), JSON_PRETTY_PRINT);
 			break;
 		case 'set_system_time':
