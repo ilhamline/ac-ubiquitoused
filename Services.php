58878d84a24151bebb00b922490bd9da15cd5b00
root <luthfi.muhammad825@gmail.com>
2016-12-31 20:32:46 +0700

    update services.php

--- Services.php
+++ Services.php
@@ -1,11 +1,13 @@
 <?php
 
 	function pingChair($baseServer){
-		$result = file_get_contents('http://10.10.100.206/?fungsi=getOccupiedSeats');
+		$result = file_get_contents('http://10.10.100.206:9000/?fungsi=getOccupiedSeats');
 		$res = json_decode($result,true);
 
-		if ($res['status'] == 'false') {
-			$hasil['status'] 'false';
+		$i = $res['status'] == 'true' ? true : false;
+
+		if (!$i) {
+			$hasil['status'] = 'false';
 			$hasil['data']['message'] = "Error saat melakukan ping";
 			return $hasil;
 		}
@@ -13,33 +15,33 @@
 		$hasil['status'] = 'true';
 		if(count($res['data'] == 0)){
 			//standby
-			$out = file_get_contents('http://'.$baseServer.'/index?fungsi=set_status&arg1=standby')
+			$out = file_get_contents('http://'.$baseServer.'/index?fungsi=set_status&arg1=standby');
 			$output = json_decode($out, true);
 
-			if($output['status'] == 'false') {
+			if($output[0]['status'] == 'false') {
 				$hasil['status'] = 'false';
 				$hasil['data']['message'] = "Gagal set standby : " . $output['data']['message'];
-				return $hasil
+				return $hasil;
 			}
 
 			$hasil['data']['message'] = "Ruangan kosong";
 		} else {
 			//on
-			$out = file_get_contents('http://'.$baseServer.'/index?fungsi=set_status&arg1=on')
-			$temp = file_get_contents('http://'.$baseServer.'/index?fungsi=set_temperature&arg1=20')
+			$out = file_get_contents('http://'.$baseServer.'/index?fungsi=set_status&arg1=on');
+			$temp = file_get_contents('http://'.$baseServer.'/index?fungsi=set_temperature&arg1=20');
 			$output = json_decode($out, true);
 			$temperature = json_decode($temp, true);
 
-			if($output['status'] == 'false') {
+			if($output[0]['status'] == 'false') {
 				$hasil['status'] = 'false';
 				$hasil['data']['message'] = "Gagal set on : " . $output['data']['message'];
-				return $hasil
+				return $hasil;
 			}
 
-			if($temperature['status'] == 'false') {
+			if($temperature[0]['status'] == 'false') {
 				$hasil['status'] = 'false';
 				$hasil['data']['message'] = "Gagal set temperature : " . $temperature['data']['message'];
-				return $hasil
+				return $hasil;
 			}
 
 			$hasil['data']['message'] = "Ruangan terisi";
